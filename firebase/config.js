c07f3d23334ed59f9bb86b735a1535a5eec9c6a9
dr250385 <dr250385@WIN-10M20X2>
2024-06-25 14:22:33 +0200

    Setting up the firebase

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBwNNP5TSAcfR_uHZMr4MaWFmTP_2uo6yE',
+  authDomain: 'udemy-vue-firebase-sites-3c78c.firebaseapp.com',
+  projectId: 'udemy-vue-firebase-sites-3c78c',
+  storageBucket: 'udemy-vue-firebase-sites-3c78c.appspot.com',
+  messagingSenderId: '413252297157',
+  appId: '1:413252297157:web:bc65061c68ab1d30e2ebac'
+};
+
+// init firebase
+firebase.initializeApp(firebase);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
