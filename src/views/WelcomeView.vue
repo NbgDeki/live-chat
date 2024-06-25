dd988f957219ead090b644d7ed61946f71753679
dr250385 <dr250385@WIN-10M20X2>
2024-06-25 15:01:27 +0200

    Created SignupForm and LoginForm components.

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <!-- <SignupForm></SignupForm> -->
+    <login-form></login-form>
   </div>
 </template>
 
 <script>
+import SignupForm from '@/components/SignupForm.vue';
+import LoginForm from '@/components/LoginForm.vue';
+
 export default {
-  name: 'Welcome'
+  name: 'Welcome',
+  components: { SignupForm, LoginForm }
 };
 </script>
 
@@ -14,5 +20,25 @@ export default {
 .welcome {
   text-align: center;
   padding: 20px 0;
+
+  form {
+    width: 300px;
+    margin: 20px auto;
+  }
+
+  label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+
+  input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    columns: #999;
+    margin: 10px auto;
+  }
 }
 </style>
