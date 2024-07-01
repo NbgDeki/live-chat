1e9e89e29bdf6add3cb9dfe1573f1b6cda27997b
dr250385 <dr250385@WIN-10M20X2>
2024-07-01 12:59:37 +0200

    Added NewChatForm component

--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <navbar></navbar>
+    <new-chat-form></new-chat-form>
   </div>
 </template>
 
@@ -9,11 +10,12 @@ import Navbar from '@/components/Navbar.vue';
 import getUser from '@/composables/getUser';
 import { watch } from 'vue';
 import { useRouter } from 'vue-router';
+import NewChatForm from '@/components/NewChatForm.vue';
 
 export default {
   name: 'Chatroom',
 
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
 
   setup() {
     const { user } = getUser();
